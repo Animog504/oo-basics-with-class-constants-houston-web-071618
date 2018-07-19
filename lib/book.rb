18ae4914d1dcb5432fd04c0a4e9a5ed810ef8b2f
Scott Ungchusri <animoogle504@gmail.com>
2018-07-19 03:56:53 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,9 +2,15 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  @@genreArray = []
+
   def initialize(title)
     @title = title
   end
+  def genre=(genreInput)
+    @genre = genreInput
+    @@genreArray << @genre
+  end 
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
